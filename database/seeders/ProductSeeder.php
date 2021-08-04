92b5829aac1a6f7a48e5f5faa7448e982e941635
Your Name <thangpdth2009031@fpt.edu.vn>
2021-08-04 10:51:02 +0700

    edit seed

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,7 +15,7 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+//        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         DB::table('products')->truncate();
         DB::table('products')->insert([
             [
@@ -79,6 +79,6 @@ class ProductSeeder extends Seeder
                 'created_at' => Carbon::now(-3)
             ]
         ]);
-        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+//        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
